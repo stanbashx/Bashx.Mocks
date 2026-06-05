611e3d8b94ef43da3f960a53b89667cc566153f2
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-06 00:33:15 +0300

    tests

--- src/test/bash/stat/bin/stat_test.sh
+++ src/test/bash/stat/bin/stat_test.sh
@@ -1,3 +1,20 @@
 #!/usr/local/bin/bash
 
+SCRIPT='src/main/bash/stat/bin/stat'
+
+echo "Running test of \"${SCRIPT}\"..."
+
+. $asserts/files/execs.sh "${SCRIPT}"
+
+if ! /usr/local/bin/bash -n "${SCRIPT}"; then
+ echo "\"${SCRIPT}\" has wrong syntax!" >&2; exit 1; fi
+
+STDERR="$(mktemp)"
+STDOUT="$(mktemp)"
+
+"${SCRIPT}" >"${STDOUT}" 2>"${STDERR}"
+. $asserts/strings/eq.sh "${SCRIPT}" "$?" '0'
+. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" ''
+. $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDOUT}")" ''
+
 echo 'Not implemented!'; exit 1 # todo
