1c2647166a95b745225b69d97d052376866bbe4b
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-06 15:44:05 +0300

    tests

---
+++ src/test/bash/curl/bin/curl_test.sh
@@ -0,0 +1,48 @@
+#!/usr/local/bin/bash
+
+SCRIPT='src/main/bash/curl/bin/curl'
+
+echo "Running test of \"${SCRIPT}\"..."
+
+. $asserts/files/execs.sh "${SCRIPT}"
+
+if ! /usr/local/bin/bash -n "${SCRIPT}"; then
+ echo "\"${SCRIPT}\" has wrong syntax!" >&2; exit 1; fi
+
+STDERR="$(mktemp)"
+STDOUT="$(mktemp)"
+
+PATH="src/main/bash/curl/bin:${PATH}" \
+ curl >"${STDOUT}" 2>"${STDERR}"
+. $asserts/strings/eq.sh "${SCRIPT}" "$?" '0'
+. $asserts/files/empty.sh "${STDERR}"
+. $asserts/files/empty.sh "${STDOUT}"
+
+EXIT_CODES=(0 256 'x' '01' $'0\n')
+for MOCKS_CURL_EXIT_CODE in "${EXIT_CODES[@]}"; do
+ :> "${STDERR}"
+ :> "${STDOUT}"
+ PATH="src/main/bash/curl/bin:${PATH}" \
+ MOCKS_CURL_EXIT_CODE="${MOCKS_CURL_EXIT_CODE}" \
+  curl >"${STDOUT}" 2>"${STDERR}"
+ . $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
+ . $asserts/strings/eq.sh "${SCRIPT}" "$(<"${STDERR}")" 'Wrong exit code!'
+ . $asserts/files/empty.sh "${STDOUT}"
+done
+
+EXIT_CODES=(1 42 255)
+for MOCKS_CURL_EXIT_CODE in "${EXIT_CODES[@]}"; do
+ :> "${STDERR}"
+ :> "${STDOUT}"
+ PATH="src/main/bash/curl/bin:${PATH}" \
+ MOCKS_CURL_EXIT_CODE="${MOCKS_CURL_EXIT_CODE}" \
+  curl >"${STDOUT}" 2>"${STDERR}"
+ . $asserts/strings/eq.sh "${SCRIPT}" "$?" "${MOCKS_CURL_EXIT_CODE}"
+ . $asserts/files/empty.sh "${STDERR}"
+ . $asserts/files/empty.sh "${STDOUT}"
+done
+
+echo 'Not implemented!'; exit 1 # todo
+
+rm "${STDERR}"
+rm "${STDOUT}"
