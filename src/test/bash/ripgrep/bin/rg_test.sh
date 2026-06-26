435451a0846b689470c8eb6e2b06287a3d77f161
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-26 16:13:56 +0300

    tests

---
+++ src/test/bash/ripgrep/bin/rg_test.sh
@@ -0,0 +1,52 @@
+#!/usr/local/bin/bash
+
+SCRIPT='src/main/bash/ripgrep/bin/rg'
+
+echo "Running test for \"${SCRIPT}\"..."
+
+. $asserts/files/execs.sh "${SCRIPT}"
+
+if ! /usr/local/bin/bash -n "${SCRIPT}"; then
+ echo "\"${SCRIPT}\" has invalid syntax!" >&2; exit 1; fi
+
+STDOUT="$(mktemp)"
+STDERR="$(mktemp)"
+
+#
+
+:> "${STDOUT}"
+:> "${STDERR}"
+PATH="src/main/bash/ripgrep/bin:${PATH}" \
+ rg > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 0
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/empty.sh "${STDERR}"
+
+EXIT_CODES=(0 256 'x' '01' $'0\n')
+for MOCKS_RIPGREP_EXIT_CODE in "${EXIT_CODES[@]}"; do
+ :> "${STDERR}"
+ :> "${STDOUT}"
+ PATH="src/main/bash/ripgrep/bin:${PATH}" \
+ MOCKS_RIPGREP_EXIT_CODE="${MOCKS_RIPGREP_EXIT_CODE}" \
+  rg > "${STDOUT}" 2> "${STDERR}"
+ . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+ . $asserts/files/empty.sh "${STDOUT}"
+ . $asserts/files/equals.sh "${STDERR}" $'Wrong exit code!\n'
+done
+
+EXIT_CODES=(1 42 255)
+for MOCKS_RIPGREP_EXIT_CODE in "${EXIT_CODES[@]}"; do
+ :> "${STDERR}"
+ :> "${STDOUT}"
+ PATH="src/main/bash/ripgrep/bin:${PATH}" \
+ MOCKS_RIPGREP_EXIT_CODE="${MOCKS_RIPGREP_EXIT_CODE}" \
+  rg > "${STDOUT}" 2> "${STDERR}"
+ . $asserts/ints/eq.sh "${SCRIPT}" "$?" "${MOCKS_RIPGREP_EXIT_CODE}"
+ . $asserts/files/empty.sh "${STDOUT}"
+ . $asserts/files/empty.sh "${STDERR}"
+done
+
+#
+
+rm "${STDERR}"
+rm "${STDOUT}"
