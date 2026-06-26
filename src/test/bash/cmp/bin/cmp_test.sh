55c4c59ba9624e962f8b3beb0a79520fcc775407
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-26 17:01:52 +0300

    wip

--- src/test/bash/cmp/bin/cmp_test.sh
+++ src/test/bash/cmp/bin/cmp_test.sh
@@ -14,20 +14,22 @@ STDERR="$(mktemp)"
 
 #
 
+:> "${STDOUT}"
+:> "${STDERR}"
 PATH="src/main/bash/cmp/bin:${PATH}" \
-cmp >"${STDOUT}" 2>"${STDERR}"
-. $asserts/strings/eq.sh "${SCRIPT}" "$?" '0'
+ cmp > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 0
 . $asserts/files/empty.sh "${STDOUT}"
 . $asserts/files/empty.sh "${STDERR}"
 
-EXIT_CODES=(0 256 'x' '01' $'0\n' '-1' '+1' ' 1' 2147483647)
+EXIT_CODES=('' ' ' $'\n' $'\t' '-0' '0' '+0' '256' 'x' '01' $'0\n' '-1' ' 1' '+1'  -2147483648 -2147483649 2147483647 2147483648)
 for MOCKS_CMP_EXIT_CODE in "${EXIT_CODES[@]}"; do
  :> "${STDOUT}"
  :> "${STDERR}"
  PATH="src/main/bash/cmp/bin:${PATH}" \
- MOCKS_CMP_EXIT_CODE="${MOCKS_CMP_EXIT_CODE}" \
- cmp >"${STDOUT}" 2>"${STDERR}"
- . $asserts/strings/eq.sh "${SCRIPT}" "$?" '1'
+  MOCKS_CMP_EXIT_CODE="${MOCKS_CMP_EXIT_CODE}" \
+  cmp > "${STDOUT}" 2> "${STDERR}"
+ . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
  . $asserts/files/empty.sh "${STDOUT}"
  . $asserts/files/equals.sh "${STDERR}" $'Wrong exit code!\n'
 done
@@ -37,9 +39,9 @@ for MOCKS_CMP_EXIT_CODE in "${EXIT_CODES[@]}"; do
  :> "${STDOUT}"
  :> "${STDERR}"
  PATH="src/main/bash/cmp/bin:${PATH}" \
- MOCKS_CMP_EXIT_CODE="${MOCKS_CMP_EXIT_CODE}" \
- cmp >"${STDOUT}" 2>"${STDERR}"
- . $asserts/strings/eq.sh "${SCRIPT}" "$?" "${MOCKS_CMP_EXIT_CODE}"
+  MOCKS_CMP_EXIT_CODE="${MOCKS_CMP_EXIT_CODE}" \
+  cmp > "${STDOUT}" 2> "${STDERR}"
+ . $asserts/ints/eq.sh "${SCRIPT}" "$?" "${MOCKS_CMP_EXIT_CODE}"
  . $asserts/files/empty.sh "${STDOUT}"
  . $asserts/files/empty.sh "${STDERR}"
 done
